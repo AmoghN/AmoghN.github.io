c9faa831ef28a22f5aceead97ac6ab54c96175ee
Amogh Nayak <amoghnk03@gmail.com>
2015-12-31 22:12:45 -0500

    major launch: full website

--- js/script.js
+++ js/script.js
@@ -1,21 +1,18 @@
-// $(function() {
-//     var bg = $('#header_image');
-//     var backgrounds = ['url(./header_image1.jpg)','url(./header_image2.jpg)'];
-//     var current = 0;
-
-// function nextBackground() {
-//   bg.css(
-//    'background-image',
-//     backgrounds[current = ++current % backgrounds.length]
-//  );
-
-//  setTimeout(nextBackground, 5000);
-//  }
-//  setTimeout(nextBackground, 5000);
-//    bg.css('background-image', backgrounds[0]);
-//  });
-
 function scrollToAnchor(def){
     var location = $(def);
     $('html,body').animate({scrollTop: location.offset().top},'slow');
 }
+
+
+$(document).ready(function () {
+	var navOffset = 0;
+	$(window).scroll(function() {
+		var scrollpos = $(window).scrollTop();
+		if (scrollpos > navOffset){
+			$("nav").addClass("fixed");
+		}else{
+			$("nav").removeClass("fixed");
+		}
+	})
+	
+})
