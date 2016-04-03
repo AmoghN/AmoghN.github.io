b5475f2b11a2ee0f869055ec1dd91d2c12e4fdd9
Amogh Nayak <amoghnk03@gmail.com>
2016-04-02 20:37:51 -0400

    Added links to projects

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
-function scrollToAnchor(def){
-    var location = $(def);
-    $('html,body').animate({scrollTop: location.offset().top},'slow');
+// scroll page to dedicated location
+function scrollToAnchor(location){
+    $('html,body').animate({scrollTop: $(location).offset().top - 75},'slow');
 }
+
+// change navbar to white when user scrolls
 $(document).ready(function () {
-	var navOffset = 0;
+	var navOffset = 50;
 	$(window).scroll(function() {
 		var scrollpos = $(window).scrollTop();
 		if (scrollpos > navOffset){
